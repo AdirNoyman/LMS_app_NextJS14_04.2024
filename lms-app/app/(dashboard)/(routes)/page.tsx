90b03d2e52b54f4dda942205aa3dfd213bfc3817
Adir Noyman <ofahol2007@hotmail.com>
2024-04-14 21:18:45 +0300

    first commit

---
+++ lms-app/app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl font-medium text-sky-700">Hello Adiros 🥳</h1>
+      <Button variant="default">Click me</Button>
+    </>
+  );
+}
