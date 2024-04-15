9b89a04bf73806c3e51bca95aa728ea8d3c70abf
Adir Noyman <ofahol2007@hotmail.com>
2024-04-15 20:54:53 +0300

    added Clerk auth

--- lms-app/app/(dashboard)/(routes)/page.tsx
+++ lms-app/app/(dashboard)/(routes)/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-medium text-sky-700">Hello Adiros 🥳</h1>
-      <Button variant="default">Click me</Button>
+      <UserButton afterSignOutUrl="/" />
     </>
   );
 }
